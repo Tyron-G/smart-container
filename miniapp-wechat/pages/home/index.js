ea353945a79b3801c5644ba79a7d71fd425508d2
Tyron <18132077408@163.com>
2026-06-01 13:12:08 +0800

    feat: polish wechat miniapp home page

--- miniapp-wechat/pages/home/index.js
+++ miniapp-wechat/pages/home/index.js
@@ -1,10 +1,33 @@
+const { getSession, setSession } = require('../../utils/session')
+
 Page({
   data: {
-    customerIdText: 'CUST-10001'
+    customerIdText: 'CUST-10001',
+    loggedIn: false
+  },
+
+  onShow() {
+    const session = getSession()
+    this.setData({
+      customerIdText: session && session.customerId ? session.customerId : 'CUST-10001',
+      loggedIn: !!(session && session.customerId)
+    })
   },
 
   demoLogin() {
-    this.setData({ customerIdText: 'CUST-10001' })
+    const session = {
+      customerId: 'CUST-10001',
+      mobile: '13800010001',
+      openId: 'demo-openid',
+      bindStatus: true,
+      accessToken: 'local-demo-token'
+    }
+    setSession(session)
+    this.setData({
+      customerIdText: session.customerId,
+      loggedIn: true
+    })
+    wx.showToast({ title: '已登录', icon: 'success' })
   },
 
   goLogin() {
